f4cd67e3eac502bdc85d5c100ff9663a2d90f479
TR Jordan <trjordan@t-sciences.com>
2010-10-18 15:40:46 -0400

    Broke jslint comments header out into separate file.

--- run_jslint.sh
+++ run_jslint.sh
@@ -2,6 +2,6 @@
 
 path=`dirname $0`
 
-echo -n /*jslint white: false, forin: true, maxlen: 120, regexp: false, newcap: true*/ /*global Ext: false, TST: false*/ | cat - $1 > $path/tmp.js
+cat $path/jslint-comment-header $1 > $path/tmp.js
 rhino $path/jslint.js $path/tmp.js
-rm $path/tmp.js
+#rm $path/tmp.js
