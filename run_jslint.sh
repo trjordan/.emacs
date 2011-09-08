66915b00034349d58f294f5cdb435f1754eb9a45
TR Jordan <tr@tracelytics.com>
2011-09-08 14:58:36 +0000

    Updated ignore list to jslint, and fixed naming on tmp js files.

--- run_jslint.sh
+++ run_jslint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+#
+# Runs JSLint on a given file, emitting warnings from the tracelytics-specific
+# options file.
 
 path=`dirname $0`
 
-cat $path/jslint-comment-header | tr '\n' ' ' > jslint-comment-header-oneline
-cat jslint-comment-header-oneline $1 > $path/tmp.js
+cat $path/jslint-comment-header | tr '\n' ' ' > $path/jslint-comment-header-oneline
+cat $path/jslint-comment-header-oneline $1 > $path/tmp.js
 rhino $path/jslint.js $path/tmp.js
 rm $path/tmp.js
